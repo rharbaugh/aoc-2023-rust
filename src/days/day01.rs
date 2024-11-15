7938e9e3a583a92f02a04213b98eff395ade089d
Rob Harbaugh <rob@rharbaugh.com>
2024-11-15 16:40:01 -0500

    input and reading

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -1,7 +1,12 @@
-use crate::{Solution, SolutionPair};
 use std::fs::read_to_string;
 
+use crate::{Solution, SolutionPair};
+
 pub fn solve() -> SolutionPair {
+    let file_path = "input/1".to_string();
+
+    let contents = read_to_string(file_path).expect("Should have been able to read the file");
+
     let sol1: u64 = 0;
     let sol2: u64 = 0;
 
